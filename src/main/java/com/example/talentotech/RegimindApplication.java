7b3e753ad2906a364544ab97e91d4f84116ae347
adrian <adriberrio461@gmail.com>
2025-07-13 23:50:38 -0500

    base proyecto y posible excel base de datos

---
+++ src/main/java/com/example/talentotech/RegimindApplication.java
@@ -0,0 +1,13 @@
+package com.example.talentotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegimindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegimindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/talentotech/RegimindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.talentotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegimindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
